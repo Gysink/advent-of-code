6c4af64328453530b5bfcede43696182c30f5446
u216094 <kevin.gysin@sbb.ch>
2023-12-02 15:42:07 +0100

    day1 solution

---
+++ 2023/day1/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type found struct {
+	i int
+	l int
+}
+
+var searchables = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9",
+	"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	sum := getSumOfCalibrations(string(data))
+	log.Println("the result is:", sum)
+}
+
+func getSumOfCalibrations(input string) int {
+	inputByLine := splitByLine(input)
+	var sum int
+	for _, l := range inputByLine {
+		cal := getCalibrationOfLine(l)
+		sum = sum + cal
+	}
+	return sum
+}
+
+func getCalibrationOfLine(input string) int {
+	firstNumString := getFirstNum(input)
+	lastNumString := getLastNum(input)
+	return stringToInt(fmt.Sprint(firstNumString, lastNumString))
+}
+
+func stringToInt(input string) int {
+	res, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatalln("cannot convert string to int:", input)
+	}
+	return res
+}
+
+func splitByLine(input string) []string {
+	var res []string
+	splitted := strings.Split(input, fmt.Sprintln())
+	for _, s := range splitted {
+		if s != "" {
+			res = append(res, s)
+		}
+	}
+	return res
+}
+
+func getFirstNum(input string) string {
+	lowestFound := found{
+		i: 999,
+		l: 0,
+	}
+	for _, s := range searchables {
+		index := strings.Index(input, s)
+		if index != -1 && index < lowestFound.i {
+			lowestFound = found{i: index, l: len(s)}
+		}
+	}
+	result := input[lowestFound.i : lowestFound.i+lowestFound.l]
+	if lowestFound.l > 1 {
+		return replaceTextWithNumber(result)
+	}
+	return result
+}
+
+func getLastNum(input string) string {
+	highestFound := found{
+		i: 0,
+		l: 0,
+	}
+	for _, s := range searchables {
+		index := strings.LastIndex(input, s)
+		if index != -1 && index >= highestFound.i {
+			highestFound = found{i: index, l: len(s)}
+		}
+	}
+	result := input[highestFound.i : highestFound.i+highestFound.l]
+	if highestFound.l > 1 {
+		return replaceTextWithNumber(result)
+	}
+	return result
+}
+
+func replaceTextWithNumber(input string) string {
+	switch input {
+	case "one":
+		return "1"
+	case "two":
+		return "2"
+	case "three":
+		return "3"
+	case "four":
+		return "4"
+	case "five":
+		return "5"
+	case "six":
+		return "6"
+	case "seven":
+		return "7"
+	case "eight":
+		return "8"
+	case "nine":
+		return "9"
+	default:
+		return ""
+	}
+}
---
+++ 2023/day1/main_test.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetFirstNum(t *testing.T) {
+	input := "1abc2"
+	want := "1"
+	got := getFirstNum(input)
+	if got != want {
+		t.Errorf("wrong first num, got=%s want=%s", got, want)
+	}
+}
+
+func TestGetLastNum(t *testing.T) {
+	input := "1abc2"
+	want := "2"
+	got := getLastNum(input)
+	if got != want {
+		t.Errorf("wrong first num, got=%s want=%s", got, want)
+	}
+}
+
+func TestStringToInt(t *testing.T) {
+	input := "12"
+	want := 12
+	got := stringToInt(input)
+	if got != want {
+		t.Errorf("wrong convertion, got=%d, want=%d", got, want)
+	}
+}
+
+func TestSplitByLine(t *testing.T) {
+	input := "these\n" +
+		"are\n" +
+		"some\n" +
+		"lines\n"
+	want := []string{"these", "are", "some", "lines"}
+	got := splitByLine(input)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("wrong splitByLine, got=%s, want=%s", got, want)
+	}
+}
+
+func TestGetCalibrationOfLine(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{name: "first", input: "1abc2", want: 12},
+		{name: "second", input: "pqr3stu8vwx", want: 38},
+		{name: "multiple numbers", input: "a1b2c3d4e5f", want: 15},
+		{name: "one number", input: "treb7uchet", want: 77},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getCalibrationOfLine(tt.input)
+			if got != tt.want {
+				t.Errorf("wrong calibration, got=%d, want=%d", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetSumOfCalibrations(t *testing.T) {
+	input := "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
+	want := 142
+	got := getSumOfCalibrations(input)
+	if got != want {
+		t.Errorf("wrong sum, got=%d, want=%d", got, want)
+	}
+}
+
+// part 2
+
+func TestGetRealFirstNum(t *testing.T) {
+	input := "two1nine"
+	want := "2"
+	got := getFirstNum(input)
+	if got != want {
+		t.Errorf("wrong first real num, got=%s want=%s", got, want)
+	}
+}
+
+func TestGetRealLastNum(t *testing.T) {
+	input := "two1nine"
+	want := "9"
+	got := getLastNum(input)
+	if got != want {
+		t.Errorf("wrong first real num, got=%s want=%s", got, want)
+	}
+}
+
+func TestGetRealCalibrationOfLine(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{name: "1", input: "two1nine", want: 29},
+		{name: "2", input: "eightwothree", want: 83},
+		{name: "3", input: "abcone2threexyz", want: 13},
+		{name: "4", input: "xtwone3four", want: 24},
+		{name: "5", input: "4nineeightseven2", want: 42},
+		{name: "6", input: "zoneight234", want: 14},
+		{name: "7", input: "7pqrstsixteen", want: 76},
+		{name: "8", input: "479", want: 49},
+		{name: "9", input: "4threerrqxpmmzhrqht4two2", want: 42},
+		{name: "10", input: "threetwo1drtzsixtwofourppvg", want: 34},
+		{name: "11", input: "abc3abc", want: 33},
+		{name: "12", input: "fh5", want: 55},
+		{name: "13", input: "4one5twoxqfcflcbjqsixeightdlknnzdbzlrqfkhvm", want: 48},
+		{name: "14", input: "44kmn", want: 44},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getCalibrationOfLine(tt.input)
+			if got != tt.want {
+				t.Errorf("wrong calibration, got=%d, want=%d", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetRealSumOfCalibrations(t *testing.T) {
+	input := "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen"
+	want := 281
+	got := getSumOfCalibrations(input)
+	if got != want {
+		t.Errorf("wrong sum, got=%d, want=%d", got, want)
+	}
+}
