9d2940b01d3e95ede3b0e2ed14f7894be722fb48
u216094 <kevin.gysin@sbb.ch>
2023-12-08 17:05:24 +0100

    WIP: part2

--- 2023/day8/main.go
+++ 2023/day8/main.go
@@ -75,16 +75,16 @@ func solve2(input string) int64 {
 		points[p.current] = p
 	}
 
-	steps := walk3(getStartPoints(points), points, instructions)
+	steps := calcLcm(getStartPoints(points), points, instructions)
 	return steps
 }
 
-func walk3(startPoints []Point, points map[string]Point, instructions []string) int64 {
+func calcLcm(startPoints []Point, points map[string]Point, instructions []string) int64 {
 	currentPoints := startPoints
 
 	var res []int64
 	for _, p := range currentPoints {
-		res = append(res, walk22(p, points, instructions))
+		res = append(res, walkEndsWith(p, points, instructions))
 	}
 
 	sum := res[0]
@@ -95,7 +95,7 @@ func walk3(startPoints []Point, points map[string]Point, instructions []string) 
 	return sum
 }
 
-func walk22(startPoint Point, points map[string]Point, instructions []string) int64 {
+func walkEndsWith(startPoint Point, points map[string]Point, instructions []string) int64 {
 	steps := int64(0)
 	currentPoint := startPoint
 
@@ -112,57 +112,9 @@ func walk22(startPoint Point, points map[string]Point, instructions []string) in
 		}
 	}
 	if currentPoint.current[2:] != "Z" {
-		steps += walk22(currentPoint, points, instructions)
+		steps += walkEndsWith(currentPoint, points, instructions)
 	}
 	return steps
-}
-
-func walk2(startPoints []Point, points map[string]Point, instructions []string) int64 {
-	var steps int64
-	steps = 0
-	currentPoints := startPoints
-
-	for !allEndWithZ(currentPoints) {
-		for _, in := range instructions {
-			if in == "R" {
-				for i, cp := range currentPoints {
-					currentPoints[i] = points[cp.right]
-				}
-				steps++
-			} else if in == "L" {
-				for i, cp := range currentPoints {
-					currentPoints[i] = points[cp.left]
-				}
-				steps++
-			}
-			if allEndWithZ(currentPoints) {
-				break
-			}
-		}
-	}
-	//if !allEndWithZ(currentPoints) {
-	//	//log.Println("next round:", steps)
-	//	steps += walk2(currentPoints, points, instructions)
-	//}
-	return steps
-}
-
-func allEndWithZ(points []Point) bool {
-	for _, p := range points {
-		if p.current[2:] != "Z" {
-			return false
-		}
-	}
-	return true
-}
-
-func oneEndsWithZ(points []Point) bool {
-	for _, p := range points {
-		if p.current[2:] == "Z" {
-			return true
-		}
-	}
-	return false
 }
 
 func getStartPoints(points map[string]Point) []Point {
